85e3e6c29d3ea4ec918b5f5d820e3c2c26473eb4
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2023-12-04 00:11:07 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/game/tictactoe/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.game.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent iHome = new Intent(SplashScreen.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(iHome);
+                finish();
+            }
+        },5500);
+    }
+}
